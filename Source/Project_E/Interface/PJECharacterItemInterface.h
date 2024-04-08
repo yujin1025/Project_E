56e0eac183b548d917fddd01b13d39637a86685b
claire1025 <claire1025@naver.com>
2024-04-08 14:16:15 +0900

    아이템, 인벤토리 UI 구현중

--- Source/Project_E/Interface/PJECharacterItemInterface.h
+++ Source/Project_E/Interface/PJECharacterItemInterface.h
@@ -23,5 +23,5 @@ class PROJECT_E_API IPJECharacterItemInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	virtual void TakeItem(UItem* Item) = 0;
+	//virtual void TakeItem(UItem* Item) = 0;
 };
