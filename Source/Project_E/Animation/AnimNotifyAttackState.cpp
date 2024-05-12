1b2c3d11e9479122e639ac3d79905a80f31e3c1c
claire1025 <claire1025@naver.com>
2024-05-12 17:28:54 +0900

    damage 구현 완료 (애니메이션 나온뒤 더 수정 가능)
    animnotifyattackstate에 데미지 깎이는 구현을 완료했으므로 공격하는 몽타주에 notify state로 넣어버리면 캐릭터든 몬스터든 데미지 깎임

---
+++ Source/Project_E/Animation/AnimNotifyAttackState.cpp
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotifyAttackState.h"
+#include "../Character/PJECharacterBase.h"
+#include "../Character/Component/HealthComponent.h"
+
+
+//공격 범위 내에 있는 대상을 찾아 데미지를 가함
+void UAnimNotifyAttackState::TryAttack(USkeletalMeshComponent* MeshComp)
+{
+	if (CurrentAttackCount >= 1)
+		return;
+
+	APJECharacterBase* MyCharacter = Cast<APJECharacterBase>(MeshComp->GetOwner());
+	TArray<APJECharacterBase*> TargetCharacters;
+
+	if (TryGetOverlapTargets(MyCharacter, TargetCharacters))
+	{
+		for (auto* TargetCharacter : TargetCharacters)
+		{
+			UHealthComponent* DamagedHealthComponent = TargetCharacter->FindComponentByClass<UHealthComponent>();
+			if (DamagedHealthComponent)
+			{
+				DamagedHealthComponent->ChangeHealth(-DamageAmount);
+			}
+		}
+	}
+}
+
+void UAnimNotifyAttackState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	CurrentAttackCount = 0;
+	TryAttack(MeshComp);
+}
+
+void UAnimNotifyAttackState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	TryAttack(MeshComp);
+}
+
+void UAnimNotifyAttackState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	CurrentAttackCount = 0;
+}
+
+//주어진 캐릭터의 위치를 중심으로 범위 내에 있는 캐릭터를 검색
+bool UAnimNotifyAttackState::TryGetOverlapResult(APJECharacterBase* Owner, TArray<FOverlapResult>& OverlapResults)
+{
+	auto* World = Owner ? Owner->GetWorld() : nullptr;
+	if (World == nullptr)
+		return false;
+
+	FVector Center = Owner->GetActorLocation();
+	FCollisionQueryParams CollisionParam(NAME_None, false, Owner);
+
+	bool bResult = World->OverlapMultiByChannel(
+		OverlapResults,
+		Center + CenterOffset,
+		Owner->GetActorQuat(),
+		ECollisionChannel::ECC_GameTraceChannel2,
+		FCollisionShape::MakeSphere(600.0f),
+		CollisionParam);
+
+	return bResult;
+}
+
+//주어진 캐릭터를 중심으로 공격 범위 내에 있는 대상을 검색
+bool UAnimNotifyAttackState::TryGetOverlapTargets(APJECharacterBase* Character, OUT TArray<APJECharacterBase*>& FoundTargets)
+{
+	TArray<FOverlapResult> OverlapResults;
+	bool bResult = false;
+
+	if (TryGetOverlapResult(Character, OverlapResults))
+	{
+		for (auto const& OverlapResult : OverlapResults)
+		{
+			auto* TargetCharacter = GetCharacter(OverlapResult);
+			if (TargetCharacter == nullptr)
+				continue;
+
+			bResult = true;
+			FoundTargets.Add(TargetCharacter);
+			break;
+		}
+	}
+
+	return bResult;
+}
--- Source/Project_E/Animation/PJEAnimNotifyState.cpp
+++ Source/Project_E/Animation/PJEAnimNotifyState.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Animation/PJEAnimNotifyState.h"
+#include "PJEAnimNotifyState.h"
+#include "../Character/PJECharacterBase.h"
 
 void UPJEAnimNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
 {
@@ -13,4 +14,18 @@ void UPJEAnimNotifyState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequ
 
 void UPJEAnimNotifyState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
 {
+}
+
+APJECharacterBase* UPJEAnimNotifyState::GetCharacter(const FOverlapResult& OverlapResult)
+{
+	auto* Actor = OverlapResult.GetActor();
+	if (Actor == nullptr)
+		return nullptr;
+
+	return Cast<APJECharacterBase>(Actor);
+}
+
+APJECharacterBase* UPJEAnimNotifyState::GetCharacter(USkeletalMeshComponent* MeshComp)
+{
+	return Cast<APJECharacterBase>(MeshComp->GetOwner());
 }
